e9c13b2c4bf1299591bef3b67e48b4d85197ff23
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2021-04-24 20:16:50 +0900

    コメントを丁寧に

--- setup-test-framework.js
+++ setup-test-framework.js
@@ -1,4 +1,4 @@
 const registerAllureReporter = require("jest-allure/dist/setup").registerAllureReporter;
 const registerAllureImageSnapshot = require("jest-allure-image-snapshot").registerAllureImageSnapshot;
-// registerAllureReporter();
+// registerAllureReporter(); // 自分で呼ばなくてもjest-allure/dist/setupをrequireするだけで大丈夫
 registerAllureImageSnapshot({customSnapshotsDir: './snapshots'});
