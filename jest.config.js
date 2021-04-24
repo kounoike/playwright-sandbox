1d8c2ee64d814e7852c8a559bdb8aa7514ad7c5c
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2021-04-24 16:46:22 +0900

    ブラウザ3種使ってみる

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,35 @@
 module.exports = {
     preset: 'jest-playwright-preset',
     setupFilesAfterEnv: ['./setup-test-framework.js'],
-    testRunner : 'jasmine2',
+    testRunner: 'jasmine2',
+    testEnvironmentOptions: {
+        'jest-playwright': {
+            // Options...
+            launchOptions: {
+                headless: false
+            },
+            browsers: [
+                {
+                    name: 'chromium',
+                    launchOptions: {
+                        channel: 'chrome',
+                        headless: false
+                    }
+                },
+                {
+                    name: 'chromium',
+                    launchOptions: {
+                        channel: 'msedge',
+                        headless: false
+                    }
+                },
+                {
+                    name: 'firefox',
+                    launchOptions: {
+                        headless: false
+                    }
+                }
+            ]
+        },
+    },
 };
